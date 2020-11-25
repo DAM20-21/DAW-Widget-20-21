821f9b0e06d8f22eccfd5cf66431f4d40213c558
Ricardo Martin Garcia <rmarting52@instituto.extremadura.es>
2020-11-25 19:21:45 +0100

    Colocar elementos

--- src/martin_garcia_ricardo/Principal.java
+++ src/martin_garcia_ricardo/Principal.java
@@ -1,5 +1,8 @@
 package martin_garcia_ricardo;
 
 public class Principal {
-    
+    public static void main(String[] args) {
+        WidgetRicardo wr = new WidgetRicardo();
+        wr.setVisible(true);
+    }
 }
--- src/martin_garcia_ricardo/WidgetRicardo.java
+++ src/martin_garcia_ricardo/WidgetRicardo.java
@@ -1,5 +1,50 @@
 package martin_garcia_ricardo;
 
-public class WidgetRicardo {
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+
+public class WidgetRicardo extends JDialog {
+
+    final static private int SOL = 1;
+    final static private int NUBE = 2;
+    final static private int LLUVIA = 3;
+    final static private int NIEBLA = 4;
+
+    JLabel hora;
+    JPanel imagen;
+    JLabel tiempoQhace;
+    JLabel temperaturas;
+
+public WidgetRicardo(){
+    super();
+    setModal(true);
+    setBounds(0,0,400,600);
+    hora = new JLabel("00:00");
+    imagen = new JPanel();
+    tiempoQhace = new JLabel("TIMEPO_Q_HACE");
+    temperaturas = new JLabel("TEMPERATURAS");
+    setLayout(new GridBagLayout());
+    añadirComponentes();
+
+}
+
+private void añadirComponentes() {
+    //Ponemos el marcador de la hora;
+    GridBagConstraints ajustes = new GridBagConstraints();
+    ajustes.gridx = 0;
+    ajustes.gridy = 0;
+    add(hora,ajustes);
+    //Ponemos el panel de la imagen 
+    ajustes.gridy = 1;
+    imagen.setBackground(Color.BLUE);
+    add(imagen,ajustes);
+
+}
+
+
     
 }
