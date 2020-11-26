52eb0d28e562e791ab07a9fffa3365a28683ff11
Alberto Ventura Hermoso <aventurah01@instituto.extremadura.es>
2020-11-26 15:58:00 +0100

    Primeros pasos creando la interfaz

--- src/ventura_hermoso_alberto/Principal.java
+++ src/ventura_hermoso_alberto/Principal.java
@@ -1,7 +1,18 @@
 package ventura_hermoso_alberto;
 
+import java.awt.EventQueue;
+
 public class Principal {
     public static void main(String[] args) {
-
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    VentanaPrincipal ventana = new VentanaPrincipal();
+                    ventana.inicializar();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- src/ventura_hermoso_alberto/VentanaPrincipal.java
+++ src/ventura_hermoso_alberto/VentanaPrincipal.java
@@ -1,11 +1,8 @@
 package ventura_hermoso_alberto;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.ImageIcon;
+import javax.swing.*;
 import java.awt.image.*;
-import java.awt.GridLayout;
+import java.awt.*;
 
 public class VentanaPrincipal {
     private JFrame ventana;
@@ -15,12 +12,39 @@ public class VentanaPrincipal {
 
     public VentanaPrincipal() {
         ventana = new JFrame();
-        ventana.setBounds(100, 100, 100, 100);
+        ventana.setBounds(100, 100, 300, 300);
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void inicializarComnponentes() {
-        ventana.setLayout(new GridLayout(2, 1));
+        ventana.setLayout(new GridBagLayout());
         panel_imagen = new JPanel();
+        panel_botones = new JPanel();
+        panel_botones.setLayout(new GridLayout(1, 4));
+        botones_acciones = new JButton[4];
+        panel_imagen.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+        panel_botones.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+
+        GridBagConstraints settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 0;
+        settings.weightx = 1;
+        settings.weighty = 1;
+        settings.fill = GridBagConstraints.BOTH;
+        ventana.add(panel_imagen, settings);
+
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 1;
+        settings.weightx = 1;
+        settings.weighty = 1;
+        settings.fill = GridBagConstraints.BOTH;
+        ventana.add(panel_botones, settings);
+
+    }
+
+    public void inicializar() {
+        ventana.setVisible(true);
+        inicializarComnponentes();
     }
 }
