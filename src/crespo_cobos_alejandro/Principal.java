ca8d4dd6355659487ed817c6b4d37ec1594a36df
Alejandro Crespo Cobos <acrespoc07@instituto.extremadura.es>
2020-11-26 16:10:16 +0100

    Cambios Principal

--- src/crespo_cobos_alejandro/Principal.java
+++ src/crespo_cobos_alejandro/Principal.java
@@ -1,5 +1,25 @@
 package crespo_cobos_alejandro;
 
+import java.awt.EventQueue;
+
+/**
+ * Main para probar el Widget Selector de Color
+ * @author Jesús Redondo García
+ */
 public class Principal {
-    
+
+	public static void main(String[] args) {
+
+
+		EventQueue.invokeLater(new Runnable() {
+		public void run() {
+			try {
+				VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+				ventanaPrincipal.inicializar();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	});	
+	}
 }
