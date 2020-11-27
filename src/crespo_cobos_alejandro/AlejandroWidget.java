ca6aaddc5c47542342af529d722ca304bbe65ce2
alex <alextorna99@gmailcom>
2020-11-27 17:12:06 +0100

    first commit

--- src/crespo_cobos_alejandro/AlejandroWidget.java
+++ src/crespo_cobos_alejandro/AlejandroWidget.java
@@ -2,9 +2,12 @@ package crespo_cobos_alejandro;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -13,6 +16,8 @@ import javax.swing.border.Border;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.GridLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -23,7 +28,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class AlejandroWidget extends JPanel implements AncestorListener {
+public class AlejandroWidget extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = 1L;
 
@@ -43,41 +48,41 @@ public class AlejandroWidget extends JPanel implements AncestorListener {
 
     }
 
-    private void anadirElementos(){
-         //Definimos el layout:
-         GridBagLayout gbl = new GridBagLayout();
-         GridBagConstraints opc = new GridBagConstraints();
-         this.setLayout(new GridBagLayout());
-		
-		
-		//PANEL PRINCIPAL
-		//Titulo
-	    //Panel de ciudades 
+    public void anadirElementosAlex() {
+
+        // Definimos el layout:
+        GridBagLayout gbl = new GridBagLayout();
+        GridBagConstraints opc = new GridBagConstraints();
+        this.setLayout(new GridBagLayout());
+
+        // PANEL PRINCIPAL
+        // Titulo
+        // Panel de ciudades
         opc = new GridBagConstraints();
-        panelCiudades = new JPanel(new GridLayout(1,1));
+        panelCiudades = new JPanel(new GridLayout(1, 1));
+        panelCiudades.setBorder(BorderFactory.createTitledBorder("Ciudades"));
         opc.gridx = 0;
         opc.gridy = 1;
         opc.weightx = 2;
         opc.weighty = 2;
-        opc.fill = GridBagConstraints.BOTH; 
+        opc.fill = GridBagConstraints.BOTH;
 
-        //TExto de cuidades y caracteristas del dia
-         opc = new GridBagConstraints();
+        // TExto de cuidades y caracteristas del dia
+        opc = new GridBagConstraints();
         informacion = new JTextField("BUSCA LA CIUDAD");
         informacion.setOpaque(true);
         informacion.setEnabled(true);
         informacion.setEditable(false);
-       
+
         opc.gridx = 0;
         opc.gridy = 1;
         opc.fill = GridBagConstraints.HORIZONTAL;
         panelCiudades.setBorder(BorderFactory.createEmptyBorder(80, 30, 80, 30));
         panelCiudades.add(informacion, opc);
-        
 
-        
         opc = new GridBagConstraints();
-        panelImagenes = new JPanel(new GridLayout(1,1));
+        panelImagenes = new JPanel(new GridLayout(1, 1));
+        panelImagenes.setBorder(BorderFactory.createTitledBorder("Clima"));
         opc.gridx = 1;
         opc.gridy = 1;
         opc.weightx = 2;
@@ -86,37 +91,23 @@ public class AlejandroWidget extends JPanel implements AncestorListener {
 
         opc = new GridBagConstraints();
         imagenes = new JLabel(new ImageIcon("src/imagenes/sol.png"));
-        opc.gridx= 1;
+        opc.gridx = 1;
         opc.gridy = 1;
-        
-        
+
         panelImagenes.add(imagenes, opc);
+
+    }
+
+    public void anadirListeners() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
         
-
-		
-	}
-
-    private void anadirListeners() {
-
+        anadirElementosAlex();
     }
 
-    @Override
-    public void ancestorAdded(AncestorEvent event) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void ancestorRemoved(AncestorEvent event) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void ancestorMoved(AncestorEvent event) {
-        // TODO Auto-generated method stub
-
-    }
 	
 	
 	
--- src/crespo_cobos_alejandro/VentanaPrincipal.java
+++ src/crespo_cobos_alejandro/VentanaPrincipal.java
@@ -1,11 +1,11 @@
 package crespo_cobos_alejandro;
 
-
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,147 +23,186 @@ import javax.swing.JTextField;
 
 /**
  * Ventana Principal para probar el widget Selector de Color
+ * 
  * @author Jesús Redondo García
  */
 public class VentanaPrincipal {
 
-	
-	//La ventana principal, en este caso, guarda todos los componentes:
-    private JFrame ventana;
-    
-    private JButton botonBuscar;
-    private ImageIcon imagenIcono;
+        // La ventana principal, en este caso, guarda todos los componentes:
+        private JFrame ventana;
+        AlejandroWidget alex = new AlejandroWidget();
 
-    private JTextField ciudad;
-    private JTextField informacion;
-    private JLabel imagenes;
+        private JButton botonBuscar;
+        private ImageIcon imagenIcono;
 
+        private JTextField ciudad;
+        private JTextField informacion;
+        private JLabel imagenes;
 
-    private JPanel panelInfo;
-    private JPanel panelImagenes;
-    private JPanel panelCiudades;
-    
-	
-	/**
-     * Constructor, marca el tamaño y el cierre del frame
-     */
-	public VentanaPrincipal() {
-		ventana = new JFrame();
-		ventana.setBounds(300, 150, 500, 500);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-        
-        
-	/**
-	 * Método que inicializa todos los componentes de la ventana
-	 */
-	public void inicializarComponentes(){
-		
-        //Definimos el layout:
-        GridBagLayout gbl = new GridBagLayout();
-        GridBagConstraints opc = new GridBagConstraints();
-        ventana.getContentPane().setLayout(gbl);
+        private JPanel panelInfo;
+        private JPanel panelImagenes;
+        private JPanel panelCiudades;
 
-        //Panel info
-        panelInfo = new JPanel();
-      /*   opc.gridx = 0;
-        opc.gridy = 0; */
-        
-        opc.weightx = 1;
-        opc.weighty = 1;
-        /* opc.fill = GridBagConstraints.BOTH;  */
-        opc.anchor = GridBagConstraints.NORTH;
-        ventana.getContentPane().add(panelInfo, opc); 
-        
+        /**
+         * Constructor, marca el tamaño y el cierre del frame
+         */
+        public VentanaPrincipal() {
+                ventana = new JFrame();
+                ventana.setBounds(300, 150, 600, 500);
+                ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
 
-        //text Field Ciudad (Panel info)
-        opc = new GridBagConstraints();
-        ciudad = new JTextField("BUSCA LA CIUDAD");
-        ciudad.setOpaque(true);
-        ciudad.setEditable(true);
-        ciudad.setEditable(true);
-        opc.gridx = 0;
-        opc.gridy = 0;
-       
-       /*  opc.weighty = 4;
-        opc.weightx = 1; */
-        /* opc.fill = GridBagConstraints.HORIZONTAL; */
-        panelInfo.add(ciudad, opc);
+        /**
+         * Método que inicializa todos los componentes de la ventana
+         */
+        public void inicializarComponentes() {
 
-        //BOTON BUscar ( Panel info)
-        opc = new GridBagConstraints();
-        botonBuscar = new JButton("BUSCAR");
-        opc.gridx = 1;
-        opc.gridy = 0;
-        panelInfo.add(botonBuscar, opc);
-        //añado todos mis cambios a la ventana
+                // Definimos el layout:
+                GridBagLayout gbl = new GridBagLayout();
+                GridBagConstraints opc = new GridBagConstraints();
+                ventana.getContentPane().setLayout(gbl);
 
-        
+                // Panel info
+                panelInfo = new JPanel();
+                opc.gridx = 0;
+                opc.gridy = 0;
+                opc.anchor = GridBagConstraints.NORTH;
+                panelInfo.setBorder(BorderFactory.createTitledBorder("Buscador"));
+                ventana.getContentPane().add(panelInfo, opc);
 
-/*         //Panel de ciudades 
-        opc = new GridBagConstraints();
-        panelCiudades = new JPanel(new GridLayout(1,1));
-        opc.gridx = 0;
-        opc.gridy = 1;
-        opc.weightx = 2;
-        opc.weighty = 2;
-        opc.fill = GridBagConstraints.BOTH; */
+                // text Field Ciudad (Panel info)
+                opc = new GridBagConstraints();
+                ciudad = new JTextField("BUSCA LA CIUDAD");
+                ciudad.setOpaque(true);
+                ciudad.setEditable(true);
+                ciudad.setEditable(true);
+                opc.gridx = 0;
+                opc.gridy = 0;
+                opc.fill = GridBagConstraints.HORIZONTAL;
+                panelInfo.add(ciudad, opc);
 
-        //TExto de cuidades y caracteristas del dia
-        /* opc = new GridBagConstraints();
-        informacion = new JTextField("BUSCA LA CIUDAD");
-        informacion.setOpaque(true);
-        informacion.setEnabled(true);
-        informacion.setEditable(false);
-       
-        opc.gridx = 0;
-        opc.gridy = 1;
-        opc.fill = GridBagConstraints.HORIZONTAL;
-        panelCiudades.setBorder(BorderFactory.createEmptyBorder(80, 30, 80, 30));
-        panelCiudades.add(informacion, opc);
-        ventana.getContentPane().add(panelCiudades, opc);
+                // BOTON BUscar ( Panel info)
+                opc = new GridBagConstraints();
+                botonBuscar = new JButton("BUSCAR");
+                opc.gridx = 1;
+                opc.gridy = 0;
+                panelInfo.add(botonBuscar, opc);
+                // añado todos mis cambios a la ventana
 
-        
-        opc = new GridBagConstraints();
-        panelImagenes = new JPanel(new GridLayout(1,1));
-        opc.gridx = 1;
-        opc.gridy = 1;
-        opc.weightx = 2;
-        opc.weighty = 2;
-        opc.fill = GridBagConstraints.BOTH;
+                opc = new GridBagConstraints();
+                opc.gridx = 0;
+                opc.gridy = 1;
+                opc.weightx = 5;
+                opc.weighty = 5;
+                opc.fill = GridBagConstraints.BOTH;
+                alex.setBorder(BorderFactory.createTitledBorder("Alex"));
+                ventana.getContentPane().add(alex, opc);
 
-        opc = new GridBagConstraints();
-        imagenes = new JLabel(new ImageIcon("src/imagenes/sol.png"));
-        opc.gridx= 1;
-        opc.gridy = 1;
-        
-        
-        panelImagenes.add(imagenes, opc);
-        ventana.getContentPane().add(panelImagenes, opc);
- */
+                // Panel de ciudades
+                opc = new GridBagConstraints();
+                panelCiudades = new JPanel();
+                panelCiudades.setBorder(BorderFactory.createTitledBorder("Ciudad"));
+                panelCiudades.setBackground(Color.BLUE);
+
+                
+                opc.gridx = 0;
+                opc.gridy = 0;
+                opc.weightx = 10;
+                opc.weighty = 10; 
+                opc.fill = GridBagConstraints.BOTH;
+                alex.add(panelCiudades, opc);
+
+                // Panel CLima
+                opc = new GridBagConstraints();
+                panelImagenes = new JPanel();
+                panelImagenes.setBorder(BorderFactory.createTitledBorder("Clima"));
+                panelImagenes.setBackground(Color.GREEN);
+                opc.gridx = 1;
+                opc.gridy = 0;
+               /*  opc.ipadx= 400;
+                opc.ipady = 400; */
+                
+                opc.weightx = 5;
+                opc.weighty = 5;
+                opc.fill = GridBagConstraints.BOTH;
+                alex.add(panelImagenes, opc);
 
 
+                //añado mi texto.
+                opc = new GridBagConstraints();
+                informacion = new JTextField("                                              ");
+                informacion.setEditable(false);
+                informacion.setSize(100, 100);
+                opc.gridx= 0;
+                opc.gridy = 0;
+                opc.ipady= 400;
+                panelCiudades.add(informacion, opc);
 
-        
 
-		
-	}
-	
-	/**
-	 * Método que inicializa todos los listeners del programa.
-	 */
-	public void inicializarListeners(){
-		
-		
-	}
-	
+                //añado mi imagen al panel de clima.
+                opc = new GridBagConstraints(); 
+                imagenes = new JLabel(new ImageIcon("img/sol.png")); 
 
-	/**
-	 * Método que realiza todas las llamadas necesarias para inicializar la ventana correctamente.
-	 */
-	public void inicializar(){
-		ventana.setVisible(true);
-		inicializarComponentes();	
-		//inicializarListeners();		
-	}
+                opc.gridx= 1; 
+                opc.gridy = 1;
+                
+                panelImagenes.add(imagenes, opc); 
+                
+
+
+
+
+
+
+
+
+
+
+
+                // TExto de cuidades y caracteristas del dia
+                /*
+                 * opc = new GridBagConstraints(); informacion = new
+                 * JTextField("BUSCA LA CIUDAD"); informacion.setOpaque(true);
+                 * informacion.setEnabled(true); informacion.setEditable(false);
+                 * 
+                 * opc.gridx = 0; opc.gridy = 1; opc.fill = GridBagConstraints.HORIZONTAL;
+                 * panelCiudades.setBorder(BorderFactory.createEmptyBorder(80, 30, 80, 30));
+                 * panelCiudades.add(informacion, opc);
+                 * ventana.getContentPane().add(panelCiudades, opc);
+                 * 
+                 * 
+                 * opc = new GridBagConstraints(); panelImagenes = new JPanel(new
+                 * GridLayout(1,1)); opc.gridx = 1; opc.gridy = 1; opc.weightx = 2; opc.weighty
+                 * = 2; opc.fill = GridBagConstraints.BOTH;
+                 * 
+                 * opc = new GridBagConstraints(); imagenes = new JLabel(new
+                 * ImageIcon("src/imagenes/sol.png")); opc.gridx= 1; opc.gridy = 1;
+                 * 
+                 * 
+                 * panelImagenes.add(imagenes, opc); ventana.getContentPane().add(panelImagenes,
+                 * opc);
+                 */
+
+        }
+
+        /**
+         * Método que inicializa todos los listeners del programa.
+         */
+        public void inicializarListeners() {
+                botonBuscar.addActionListener(e -> {
+                        alex.actionPerformed(e);
+
+                });
+
+        }
+
+        /**
+         * Método que realiza todas las llamadas necesarias para inicializar la ventana
+         * correctamente.
+         */
+        public void inicializar() {
+                ventana.setVisible(true);
+                inicializarComponentes();
+                inicializarListeners();
+        }
 }
