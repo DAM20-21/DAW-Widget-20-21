73bba9cbc4c0793bc3a129b48e3de7594f34c70d
Elena <einofuentem01@informatica.iesvalledeljerteplasencia.es>
2020-11-27 18:52:48 +0100

    Terminada

--- src/nofuentes_mendez_elena/Principal.java
+++ src/nofuentes_mendez_elena/Principal.java
@@ -1,18 +1,21 @@
 package nofuentes_mendez_elena;
 
 import java.awt.EventQueue;
-
 import chrriis.common.UIUtils;
 import chrriis.dj.nativeswing.swtimpl.NativeInterface;
 
 /**
  * Clase principal del reproductor de vídeo.
  * 
+ * - Native Interface es un mecanismo que nos permite interactuar con
+ * aplicaciones nativas desde un programa escrito en Java.
+ * 
  * @author Elena Nofuentes
  * @since 25-11-2020
- * @version 1.0
+ * @version 1.1
  *
  */
+
 public class Principal {
     public static void main(String[] args) throws Exception {
         NativeInterface.open();
