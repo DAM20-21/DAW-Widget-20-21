2484cfc691e7f981a5382daf9c5a93af192513c8
AnaSanchezM <71656710+AnaSanchezM@users.noreply.github.com>
2020-11-28 20:39:21 +0100

    ENTREGA FINAL

--- src/sanchez_miguel_ana/VentanaPrincipal.java
+++ src/sanchez_miguel_ana/VentanaPrincipal.java
@@ -3,12 +3,15 @@ package sanchez_miguel_ana;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.awt.GridBagLayout;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
@@ -18,6 +21,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.event.MouseInputAdapter;
 import javax.swing.plaf.InsetsUIResource;
 
@@ -33,7 +37,6 @@ public class VentanaPrincipal {
 	private JTextField insertarUsuario;
 	private JTextField insertarContra;
 	private JButton enviar;
-	// private JPanel panelColor;
 
 	/**
 	 * Constructor, marca el tamaño y el cierre del frame
@@ -62,7 +65,6 @@ public class VentanaPrincipal {
 
 		// Definimos el layout:
 		ventana.setLayout(new GridBagLayout());
-		ventana.setBackground(Color.BLUE);
 
 		// Boton iniciar sesion:
 		iniciarSesion = new JButton("Pulsa para iniciar sesión");
@@ -77,6 +79,10 @@ public class VentanaPrincipal {
 
 	}
 
+	/**
+	 * Método que inicializa los Listeners.
+	 */
+
 	public void inicializarListeners() {
 
 		iniciarSesion.addMouseListener(new MouseInputAdapter() {
@@ -84,16 +90,19 @@ public class VentanaPrincipal {
 			public void mouseClicked(MouseEvent e) {
 				GridBagConstraints settingsOptions = new GridBagConstraints();
 
+				// CREAMOS LA NUEVA VENTANA DE INTRODUCIR EL LOG-IN
 				ventanaDialog = new JFrame();
 				ventanaDialog.setLayout(new GridBagLayout());
 
 				settingsOptions = new GridBagConstraints();
 				settingsOptions.gridy = 0;
+				// CREAMOS EL TEXTO USUARIO
 				usuario = new JLabel();
 				usuario.setText("Introduce usuario: ");
 
 				ventanaDialog.add(usuario, settingsOptions);
 
+				// CREAMOS PARA INTRODUCIR EL USUARIO
 				settingsOptions = new GridBagConstraints();
 				settingsOptions.gridy = 1;
 				settingsOptions.insets = new Insets(5, 0, 0, 0);
@@ -102,6 +111,7 @@ public class VentanaPrincipal {
 				insertarUsuario = new JTextField();
 				ventanaDialog.add(insertarUsuario, settingsOptions);
 
+				// CREAMOS EL TEXTO PARA LA CONTRASEÑA
 				settingsOptions = new GridBagConstraints();
 				settingsOptions.gridy = 2;
 				settingsOptions.insets = new Insets(5, 0, 0, 0);
@@ -110,24 +120,83 @@ public class VentanaPrincipal {
 				contraseña.setText("Introduce la contraseña: ");
 				ventanaDialog.add(contraseña, settingsOptions);
 
+				// CREAMOS PARA INTRODUCIR LA CONTRASEÑA
 				settingsOptions = new GridBagConstraints();
 				settingsOptions.gridy = 3;
 				settingsOptions.fill = GridBagConstraints.HORIZONTAL;
 				settingsOptions.insets = new Insets(5, 0, 0, 0);
 
 				insertarContra = new JTextField();
-				
+
 				ventanaDialog.add(insertarContra, settingsOptions);
 
+				// HACEMOS VISIBLE LA VENTANA Y LE DAMOS UN TAMAÑO
 				ventanaDialog.setVisible(true);
 				ventanaDialog.setBounds(100, 100, 400, 400);
 
+				// INTRODUCIMOS UN BOTON PARA ENVIAR LOS DATOS
 				settingsOptions = new GridBagConstraints();
 				settingsOptions.gridy = 4;
 				settingsOptions.insets = new Insets(60, 0, 0, 0);
 				enviar = new JButton();
 				enviar.setText("Enviar");
 
+				enviar.addMouseListener(new MouseInputAdapter() {
+					@Override
+					public void mouseClicked(MouseEvent e) {
+						// ESTABLECEMOS LA CONEXIÓN
+
+						Connection conexion = WidgetAna.getConexion("mysql");
+
+						// SACAMOS DE LOS CUADROS DE TEXTO EL USUARIO Y LA CONTRASEÑA
+						String usuarioIntroducido = insertarUsuario.getText();
+						String pass = insertarContra.getText();
+
+						// PREGUNTAMOS A LA BASE DE DATOS SI EXISTE EL USUARIO CUYO NOMBRE COINCIDE CON
+						// EL INTRODUCIDO POR EL CUADRO
+						String consulta = "SELECT * FROM USUARIO WHERE NOMBRE = '" + usuarioIntroducido + "'";
+
+						// SENTENCIA SQL
+						Statement st;
+
+						try {
+							st = conexion.createStatement();
+							// EJECUTAMOS LA CONSULTA SQL
+							ResultSet rs = st.executeQuery(consulta);
+
+							// SI EL USUARIO EXISTE
+							if (rs.next()) {
+								// SE COMPRUEBA QUE COINCIDA LA CONTRASEÑA CON LA DEL USUARIO.
+								if (pass.equalsIgnoreCase(rs.getString(2))) {
+									JOptionPane.showMessageDialog(ventanaDialog, "Acceso concedido.",
+											"Log-in correcto.", 1);
+								} else {
+									JOptionPane.showMessageDialog(ventanaDialog, "Acceso denegado.", "Log-in fallido.",
+											1);
+								}
+
+							} else { // SI EL USUARIO NO EXISTE
+								// SE CREA
+								String insertar = "INSERT INTO USUARIO VALUES ( '" + usuarioIntroducido + "', '" + pass
+										+ "')";
+
+								// SE EJECUTA LA SENTENCIA
+								st = conexion.createStatement();
+								int filas = st.executeUpdate(insertar);
+								JOptionPane.showMessageDialog(ventanaDialog,
+										" Se ha creado el usuario: " + usuarioIntroducido, "El usuario no existía.", 1);
+							}
+							rs.close();
+							st.close();
+							conexion.close();
+							ventanaDialog.dispose();
+
+						} catch (SQLException e1) {
+							e1.printStackTrace();
+						}
+					}
+				});
+				// AÑADIMOS A LA VENTANA DEL DIALIOG EL BOTON
 				ventanaDialog.add(enviar, settingsOptions);
 			}
 		});
--- src/sanchez_miguel_ana/WidgetAna.java
+++ src/sanchez_miguel_ana/WidgetAna.java
@@ -1,5 +1,36 @@
 package sanchez_miguel_ana;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class WidgetAna {
-    
+    public static Connection getConexion(String bd) {
+
+        Properties propiedades = new Properties();
+        FileInputStream fis;
+        Connection conexion = null;
+        String database = bd.toLowerCase();
+
+        try {
+            // LEEMOS EL FICHERO DE CONFIGURACIÓN
+            fis = new FileInputStream("configuracion.properties");
+
+            // CARGAMOS EL ARCHIVO CON LAS PROPIEDADES DE LA BASE DE DATOS
+            propiedades.load(fis);
+
+            // CARGAMOS EL DRIVER
+            Class.forName(propiedades.getProperty("driver-" + database));
+            conexion = DriverManager.getConnection(propiedades.getProperty("conexion-" + database),
+                    propiedades.getProperty("usuario-" + database), "");
+
+        } catch (IOException | ClassNotFoundException | SQLException e) {
+
+            e.printStackTrace();
+        }
+        return conexion;
+    }
 }
