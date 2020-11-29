9f5c1a34c8940515737258691edbdc4fba9dc320
Javi Bono <jbonifacioh01@informatica.iesvalledeljerteplasencia.es>
2020-11-29 21:32:58 +0100

    Interfaz

--- src/bonifacio_hernandez_javier/VentanaPrincipal.java
+++ src/bonifacio_hernandez_javier/VentanaPrincipal.java
@@ -1,5 +1,147 @@
 package bonifacio_hernandez_javier;
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.plaf.FontUIResource;
+
+/**
+ * Ventana Principal para probar el widget Selector de Formato de Texto
+ * @author Javier Bonifacio Hernandez
+ */
 public class VentanaPrincipal {
-    
+
+	private final static String TEXTO="Cambiame el formato";
+	
+	//La ventana principal, en este caso, guarda todos los componentes:
+	private JFrame ventana;
+	private int tamañoFuente;
+    private int alineacion;
+	private int colorLetra;
+
+	public JFrame getVentana() {
+		return this.ventana;
+	}
+
+	public void setVentana(JFrame ventana) {
+		this.ventana = ventana;
+	}
+
+	public int getTamañoFuente() {
+		return this.tamañoFuente;
+	}
+
+	public void setTamañoFuente(int tamañoFuente) {
+		this.tamañoFuente = tamañoFuente;
+	}
+
+	public int getAlineacion() {
+		return this.alineacion;
+	}
+
+	public void setAlineacion(int alineacion) {
+		this.alineacion = alineacion;
+	}
+
+	public int getColorLetra() {
+		return this.colorLetra;
+	}
+
+	public void setColorLetra(int colorLetra) {
+		this.colorLetra = colorLetra;
+	}
+
+	public JButton getBDialogo() {
+		return this.bDialogo;
+	}
+
+	public void setBDialogo(JButton bDialogo) {
+		this.bDialogo = bDialogo;
+	}
+
+	public JPanel getPanelFuente() {
+		return this.panelFuente;
+	}
+
+	public void setPanelFuente(JPanel panelFuente) {
+		this.panelFuente = panelFuente;
+	}
+
+	public JLabel getJLabelFuente() {
+		return this.jLabelFuente;
+	}
+
+	public void setJLabelFuente(JLabel jLabelFuente) {
+		this.jLabelFuente = jLabelFuente;
+	}
+	
+	private JButton bDialogo;
+    private JPanel panelFuente;
+    private JLabel jLabelFuente;
+	
+	/**
+     * Constructor, marca el tamaño y el cierre del frame
+     */
+	public VentanaPrincipal() {
+		ventana = new JFrame();
+		ventana.setBounds(100, 50, 400, 200);
+		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	/**
+	 * Método que inicializa todos los componentes de la ventana
+	 */
+	public void inicializarComponentes(){
+		
+		//Definimos el layout:
+		ventana.setLayout(new GridLayout(1,2));
+		
+        bDialogo = new JButton("Selecciona la Fuente");
+		ventana.add(bDialogo);
+	
+		panelFuente = new JPanel(new GridLayout(1,1));
+        panelFuente.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+        ventana.add(panelFuente);
+
+        jLabelFuente=new JLabel(TEXTO,SwingConstants.CENTER);
+        panelFuente.add(jLabelFuente);
+		
+	}
+	
+	/**
+	 * Método que inicializa todos los listeners del programa.
+	 */
+	public void inicializarListeners(){
+		bDialogo.addActionListener(e->{
+			widgetFormatoTexto dialog = new widgetFormatoTexto(bDialogo, panelFuente.getBackground());
+			dialog.setVisible(true);
+				
+		});
+
+		cambioFormato();
+		
+	}
+	
+
+	private void cambioFormato() {
+		//jLabelFuente.setFont(new FontUIResource(TEXTO,Font.BOLD,tamañoFuente));
+	}
+
+	/**
+	 * Método que realiza todas las llamadas necesarias para inicializar la ventana
+	 * correctamente.
+	 */
+	public void inicializar(){
+		ventana.setVisible(true);
+		inicializarComponentes();	
+		inicializarListeners();		
+	}
 }
--- src/bonifacio_hernandez_javier/widgetFormatoTexto.java
+++ src/bonifacio_hernandez_javier/widgetFormatoTexto.java
@@ -1,5 +1,208 @@
 package bonifacio_hernandez_javier;
 
-public class widgetFormatoTexto {
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ * Clase que representa un diálogo selector de color. Para utilizar esta clase
+ * crea un listener del estilo:
+ * {@code  DialogSelectorColor dialog = new DialogSelectorColor(bDialogo, panelColor.getBackground());
+			dialog.addWindowListener(new WindowAdapter() { @Override public void
+ * windowClosed(WindowEvent arg0) { System.out.println(dialog.getColor()); } });
+ * } La clase está preparada para que pueda funcionar sobre cualquier
+ * componente.
+ * 
+ * @author Javier Bonifacio Hernandez
+ *
+ */
+public class widgetFormatoTexto extends JDialog implements ActionListener {
+
+    private static final long serialVersionUID = 1L;
+
+    private VentanaPrincipal ventana;
+    private int cont;
+    private int tamañoFuente;
+    private int alineacion;
+    private int formatoLetra;
+
+    private JButton bAceptar;
+    private JPanel panelTamaño;
+    private JButton[] vBotonesTamaño;
+    private JPanel panelNegrita;
+    private JButton[] vBotonesNegrita;
+    private JPanel panelAlineacion;
+    private JButton[] vBotonesAlineacion;
+
+    JLabel titulo;
+    JLabel lTamanio;
+    JLabel lColor;
+    JLabel lAlineacion;
+
+    // ColorInicial:
+    Color color = Color.BLUE;
+
+    public widgetFormatoTexto(JComponent componente, Color colorInicial) {
+        super();
+        setModal(true); // PARA HACERLO MODAL
+        setBounds((int) componente.getLocationOnScreen().getX(), (int) componente.getLocationOnScreen().getY(), 300,
+                500); // Dónde empieza el Dialog y qué tamaño tiene
+        this.color = colorInicial;
+        anadirElementos();
+        anadirListeners();
+    }
+
+    /**
+     * Método que añade todos los elementos al Dialog. Se codifica como un JFrame
+     */
+    private void anadirElementos() {
+        int contTamaño = 10;
+
+        this.setLayout(new GridBagLayout());
+
+        // PANEL PRINCIPAL
+        // Titulo
+        titulo = new JLabel("Selecciona el Formato del Texto:");
+        GridBagConstraints settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 0;
+        settings.insets = new Insets(20, 0, 50, 0);
+        add(titulo, settings);
+
+        // Tamaño
+        lTamanio = new JLabel("Tamaño del Texto:");
+        lTamanio.setLayout(new GridLayout(1, 5));
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 1;
+        add(lTamanio, settings);
+
+        panelTamaño = new JPanel();
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 2;
+        add(panelTamaño, settings);
+
+        vBotonesTamaño = new JButton[5];
+        for (int i = 0; i < vBotonesTamaño.length; i++) {
+            vBotonesTamaño[i] = new JButton(Integer.toString(contTamaño));
+            panelTamaño.add(vBotonesTamaño[i]);
+            contTamaño += 4;
+        }
+
+        // Color del texto
+        lColor = new JLabel("Formato:");
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 3;
+        add(lColor, settings);
+
+        panelNegrita = new JPanel();
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 4;
+        add(panelNegrita, settings);
+
+        vBotonesNegrita = new JButton[3];
+        vBotonesNegrita[0] = new JButton("Negrita");
+        panelNegrita.add(vBotonesNegrita[0]);
+        vBotonesNegrita[1] = new JButton("Italica");
+        panelNegrita.add(vBotonesNegrita[1]);
+        vBotonesNegrita[2] = new JButton("Subrayado");
+        panelNegrita.add(vBotonesNegrita[2]);
+
+        // Alineacion del texto
+        lAlineacion = new JLabel("Alineacion del texto:");
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 5;
+        add(lAlineacion, settings);
+
+        panelAlineacion = new JPanel();
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 6;
+        add(panelAlineacion, settings);
+
+        vBotonesAlineacion = new JButton[3];
+        vBotonesAlineacion[0] = new JButton("Izquierda");
+        panelAlineacion.add(vBotonesAlineacion[0]);
+        vBotonesAlineacion[1] = new JButton("Centro");
+        panelAlineacion.add(vBotonesAlineacion[1]);
+        vBotonesAlineacion[2] = new JButton("Derecha");
+        panelAlineacion.add(vBotonesAlineacion[2]);
+
+        // bAceptar
+        settings = new GridBagConstraints();
+        bAceptar = new JButton("Aceptar");
+        settings = new GridBagConstraints();
+        settings.gridx = 0;
+        settings.gridy = 8;
+        settings.insets = new Insets(20, 0, 20, 0);
+        add(bAceptar, settings);
+
+    }
+
+    /**
+     * Método que añade todos los listeners al Dialog
+     */
+    private void anadirListeners() {
+        cont=6;
+
+        // Tamaño
+        for (int i = 0; i < vBotonesTamaño.length; i++) {
+            cont=cont+4;
+            vBotonesTamaño[i].addActionListener(e -> {
+                tamañoFuente = cont;
     
+            });
+        }
+
+        // Formato
+        cont=0;
+        for (int i = 0; i < vBotonesNegrita.length; i++) {
+            vBotonesNegrita[cont].addActionListener(e -> {
+                formatoLetra=cont;
+    
+            });
+            cont++;
+        }
+
+        // Alineacion
+        cont=0;
+        for (int i = 0; i < vBotonesAlineacion.length; i++) {
+            vBotonesAlineacion[cont].addActionListener(e -> {
+                alineacion=cont;
+    
+            });
+            cont++;
+        }
+        
+        // Botón Aceptar:
+        bAceptar.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.dispose();
+       // ventana.setTamañoFuente(tamañoFuente);
+        
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
