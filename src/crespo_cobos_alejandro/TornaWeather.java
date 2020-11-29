44c1df358e68c6ddc12dbef061a6356b7ab4d27b
alex <alextorna99@gmailcom>
2020-11-29 13:12:23 +0100

    Cambios Widget

--- src/crespo_cobos_alejandro/TornaWeather.java
+++ src/crespo_cobos_alejandro/TornaWeather.java
@@ -36,29 +36,28 @@ public class TornaWeather extends JPanel {
 
     private static final long serialVersionUID = 1L;
 
-
     private JTextArea municipio;
     private JTextField tempM;
     private JTextField tempm;
     private JTextField cielo;
     private JLabel imagenes;
 
+    private Boolean aviso = false;
+
     private JPanel panelImagenes;
     private JPanel panelCiudades;
     ArrayList<Tiempo> pueblos = new ArrayList<>();
-    ArrayList <ImageIcon> imgs= new ArrayList<>();
-    //String [] climas = {"Soleado", "Lluvia", "Niebla", "Nieve", "Nubes y claros"};
+    ArrayList<ImageIcon> imgs = new ArrayList<>();
+    // String [] climas = {"Soleado", "Lluvia", "Niebla", "Nieve", "Nubes y
+    // claros"};
 
     // Referencia al Widget:
-   
 
     public TornaWeather() {
         super();
         anadirElementosAlex();
-        
-        
+
     }
-    
 
     public void anadirElementosAlex() {
 
@@ -68,7 +67,7 @@ public class TornaWeather extends JPanel {
         this.setLayout(new GridBagLayout());
 
         // PANEL Wiget
-     
+
         // Panel de ciudades
         opc = new GridBagConstraints();
         panelCiudades = new JPanel();
@@ -93,51 +92,41 @@ public class TornaWeather extends JPanel {
         opc.fill = GridBagConstraints.BOTH;
         this.add(panelImagenes, opc);
 
-        //Añado a mi panel Ciudades el JtextField que tendra la informacion de la ciudad
+        // Añado a mi panel Ciudades el JtextField que tendra la informacion de la
+        // ciudad
         opc = new GridBagConstraints();
         municipio = new JTextArea("");
         municipio.setBorder(BorderFactory.createTitledBorder("Municipio"));
         municipio.setEditable(false);
-        opc.gridx= 0;
+        opc.gridx = 0;
         opc.gridy = 0;
         panelCiudades.add(municipio, opc);
 
-        /* //TEMP MAXIMA
-        opc = new GridBagConstraints();
-        tempM = new JTextField("");
-        tempM.setBorder(BorderFactory.createTitledBorder("Temperatura Maxima"));
-        tempM.setEditable(false);
-        opc.gridx= 0;
-        opc.gridy = 1;
-        panelCiudades.add(tempM, opc);
+        /*
+         * //TEMP MAXIMA opc = new GridBagConstraints(); tempM = new JTextField("");
+         * tempM.setBorder(BorderFactory.createTitledBorder("Temperatura Maxima"));
+         * tempM.setEditable(false); opc.gridx= 0; opc.gridy = 1;
+         * panelCiudades.add(tempM, opc);
+         * 
+         * //TEMP MINIMA opc = new GridBagConstraints(); tempm = new JTextField("");
+         * tempm.setBorder(BorderFactory.createTitledBorder("Temperatura Minima"));
+         * tempm.setEditable(false); opc.gridx= 0; opc.gridy = 2;
+         * panelCiudades.add(tempm, opc);
+         * 
+         * //CIELO opc = new GridBagConstraints(); cielo = new JTextField("");
+         * cielo.setBorder(BorderFactory.createTitledBorder("Clima"));
+         * cielo.setEditable(false); opc.gridx= 0; opc.gridy = 3;
+         * panelCiudades.add(cielo, opc);
+         */
 
-        //TEMP MINIMA
-        opc = new GridBagConstraints();
-        tempm = new JTextField("");
-        tempm.setBorder(BorderFactory.createTitledBorder("Temperatura Minima"));
-        tempm.setEditable(false);
-        opc.gridx= 0;
-        opc.gridy = 2;
-        panelCiudades.add(tempm, opc);
-
-        //CIELO
-        opc = new GridBagConstraints();
-        cielo = new JTextField("");
-        cielo.setBorder(BorderFactory.createTitledBorder("Clima"));
-        cielo.setEditable(false);
-        opc.gridx= 0;
-        opc.gridy = 3;
-        panelCiudades.add(cielo, opc);  */
-    
-
-
-        //Añado las imagenes
+        // Añado las imagenes
         opc = new GridBagConstraints();
         imagenes = new JLabel();
-        opc.gridx= 1; 
+        opc.gridx = 1;
         opc.gridy = 1;
         panelImagenes.add(imagenes, opc);
     }
+
     /**
      * 
      */
@@ -158,28 +147,39 @@ public class TornaWeather extends JPanel {
     }
 
     /**
-     * Este metodo captura el valor que el usuario introduce y lo manda a otro JtextField para que se muestre en la interfaz
+     * Este metodo captura el valor que el usuario introduce y lo manda a otro
+     * JtextField para que se muestre en la interfaz
+     * 
+     * Tengo un booleano para comprar el aviso que salta, si el municipio que has metido no esta dentro de los pueblos del valle jerte saldra un aviso
+     * 
      * @param ciudad
      */
     public void comprobarPueblos(JTextField ciudad) {
 
         String texto = ciudad.getText();
-        //Tiempo tiempo : pueblos
-        for (int i = 0; i < pueblos.size(); i++){
+        // Tiempo tiempo : pueblos
+        for (int i = 0; i < pueblos.size(); i++) {
             if (pueblos.get(i).getNombre().equalsIgnoreCase(texto)) {
-                municipio.setText(pueblos.get(i).getNombre() + "\nTemperatura Máxima: "+pueblos.get(i).getTemperaturaMax()+ "º\nTemperatura Mínima: " + pueblos.get(i).getTemperaturaMin()+ "º\n"+ pueblos.get(i).getClima());
-
-            }else{
-              //JOptionPane.showMessageDialog(, "El municipio indicado no es correcto", "Error", JOptionPane.INFORMATION_MESSAGE);
+                municipio.setText(pueblos.get(i).getNombre() + "\nTemperatura Máxima: "
+                        + pueblos.get(i).getTemperaturaMax() + "º\nTemperatura Mínima: "
+                        + pueblos.get(i).getTemperaturaMin() + "º\n" + pueblos.get(i).getClima());
+                aviso = true;
             }
             
+                
         }
+        if(!aviso){
+            JOptionPane.showMessageDialog(this, "El municipio indicado no es correcto", "Error",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+        }
+
     }
 
     /**
      * Añado las images a mi lista
      */
-    public void anadirImagenes(){
+    public void anadirImagenes() {
 
         imgs.add(new ImageIcon("img/sol.png"));
         imgs.add(new ImageIcon("img/lluvia.png"));
@@ -188,29 +188,32 @@ public class TornaWeather extends JPanel {
         imgs.add(new ImageIcon("img/nubes.png"));
     }
 
-   
-
-    public void comprobarImagen(JTextField ciudad){
+    public void comprobarImagen(JTextField ciudad) {
 
         String texto = ciudad.getText();
-        
+
         for (int i = 0; i < pueblos.size(); i++) {
-            if(pueblos.get(i).getNombre().equalsIgnoreCase(texto) && pueblos.get(i).getClima().equalsIgnoreCase("Soleado")){
+            if (pueblos.get(i).getNombre().equalsIgnoreCase(texto)
+                    && pueblos.get(i).getClima().equalsIgnoreCase("Soleado")) {
                 imagenes.setIcon(imgs.get(0));
             }
-            if(pueblos.get(i).getNombre().equalsIgnoreCase(texto) && pueblos.get(i).getClima().equalsIgnoreCase("Lluvia")){
+            if (pueblos.get(i).getNombre().equalsIgnoreCase(texto)
+                    && pueblos.get(i).getClima().equalsIgnoreCase("Lluvia")) {
                 imagenes.setIcon(imgs.get(1));
             }
-            if(pueblos.get(i).getNombre().equalsIgnoreCase(texto) && pueblos.get(i).getClima().equalsIgnoreCase("nieve")){
+            if (pueblos.get(i).getNombre().equalsIgnoreCase(texto)
+                    && pueblos.get(i).getClima().equalsIgnoreCase("nieve")) {
                 imagenes.setIcon(imgs.get(2));
             }
-            if(pueblos.get(i).getNombre().equalsIgnoreCase(texto) && pueblos.get(i).getClima().equalsIgnoreCase("Nubes y claros")){
+            if (pueblos.get(i).getNombre().equalsIgnoreCase(texto)
+                    && pueblos.get(i).getClima().equalsIgnoreCase("Nubes y claros")) {
                 imagenes.setIcon(imgs.get(3));
             }
-            if(pueblos.get(i).getNombre().equalsIgnoreCase(texto) && pueblos.get(i).getClima().equalsIgnoreCase("Nubes")){
+            if (pueblos.get(i).getNombre().equalsIgnoreCase(texto)
+                    && pueblos.get(i).getClima().equalsIgnoreCase("Nubes")) {
                 imagenes.setIcon(imgs.get(4));
             }
 
-        }  
+        }
     }
 }
