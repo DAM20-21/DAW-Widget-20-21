4aa61c435aebd5fb4393667ff32cff40f9e1d240
sergi <sergi@LA-PARCA.home>
2020-11-30 15:27:49 +0100

    Versión 0.2 Versión del programa completo pero con un fallo en la escritura de puntuaciones.

--- src/redondo_garcia_jesus/Principal.java
+++ src/redondo_garcia_jesus/Principal.java
@@ -1,24 +1,108 @@
 package redondo_garcia_jesus;
+
 import java.awt.EventQueue;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
 
 /**
- * Main para probar el Widget Selector de Color
- * @author Jesús Redondo García
+ * Main para probar el Widget High Score
+ *
+ * @author Sergio Blanco Prieto
  */
 public class Principal {
+    
+    private static File fich = new File("puntuaciones.dat");
+    private static VentanaPrincipal ventanaPrincipal;
+    private static String[] nombres = {"Juan", "Antonio", "Ana", "Pablo", "Jorge", "Clara", "Pedro", "Alberto", "Samuel", "Sara", "Francisco", "Miguel"};
+    private static String [] pantalla;
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-
-		EventQueue.invokeLater(new Runnable() {
-		public void run() {
-			try {
-				VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
-				ventanaPrincipal.inicializar();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	});	
-	}
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ventanaPrincipal = new VentanaPrincipal();
+                    ventanaPrincipal.inicializar();
+                    escribirFich();
+                    leerFich();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    public static void escribirFich() {
+        try {
+            int puntuacion;
+            Random rd = new Random();
+            FileOutputStream fos = new FileOutputStream(fich);
+            DataOutputStream dos = new DataOutputStream(fos);
+            for (int i = 0; i < nombres.length; i++) {
+                puntuacion = rd.nextInt(100);
+                dos.writeUTF("Nuevo jugador: " + nombres[i] + "\nEl jugador " + nombres[i] + " ha conseguido " + puntuacion + " puntos.");
+                dos.writeInt(puntuacion);
+            }
+        } catch (IOException io) {
+            System.out.println("Error en las operaciones de E/S: " + io.getMessage());
+        }
+    }
+    
+    public static void leerFich() throws IOException {
+        DataInputStream dis = null;
+        pantalla = new String[5];
+        int puntuacion = 0;
+        String m1, m2;
+        try {
+            FileInputStream fis = new FileInputStream(fich);
+            dis = new DataInputStream(fis);
+            ventanaPrincipal.mitad1.setText("HIGH SCORE -->" + puntuacion + "<--");
+            int cont = 0;
+            int puntos;
+            while (true) {
+                if (cont < 5) {
+                    pantalla[cont] = dis.readUTF();
+                    actualizarPantalla(cont);
+                } else {
+                    String t = dis.readUTF();
+                    pantalla = annadirFinal(pantalla, t);
+                    actualizarPantalla(5);
+                }
+                puntos = dis.readInt();
+                if (puntos > puntuacion) {
+                    puntuacion = puntos;
+                    ventanaPrincipal.mitad1.setText("HIGH SCORE -->" + puntuacion + "<--");
+                }
+                Thread.sleep(800);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        } catch (EOFException e) {
+            System.out.println("Fin del fichero");
+        } catch (IOException io) {
+            System.out.println("Error E/S");
+        } finally {
+            dis.close();
+        }
+    }
+    
+    public static String[] annadirFinal(String [] texto, String ultimo) {
+        for (int i = 0; i < texto.length - 1; i++) {
+            texto[i] = texto[i+1];
+        }
+        texto[texto.length-1] = ultimo;
+        return texto;
+    }
+    
+    public static void actualizarPantalla(int tope) {
+        for (int i = 0; i < tope; i++) {
+            ventanaPrincipal.mitad2.setText(ventanaPrincipal.mitad2.getText() + pantalla[i] + "\n");
+        }
+    }
 }
--- src/redondo_garcia_jesus/VentanaPrincipal.java
+++ src/redondo_garcia_jesus/VentanaPrincipal.java
@@ -1,74 +1,53 @@
 package redondo_garcia_jesus;
 
-import java.awt.GridLayout;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 /**
- * Ventana Principal para probar el widget Selector de Color
- * @author Jesús Redondo García
+ * Interfaz del Widget High Score
+ *
+ * @author Sergio Blanco Prieto
  */
 public class VentanaPrincipal {
+    JFrame ventana;
+    JLabel mitad1;
+    JLabel mitad2;
 
-	
-	//La ventana principal, en este caso, guarda todos los componentes:
-	private JFrame ventana;
-	private JButton bDialogo;
-	private JPanel panelColor;
-	
-	/**
-     * Constructor, marca el tamaño y el cierre del frame
-     */
-	public VentanaPrincipal() {
-		ventana = new JFrame();
-		ventana.setBounds(100, 50, 400, 200);
-		ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	/**
-	 * Método que inicializa todos los componentes de la ventana
-	 */
-	public void inicializarComponentes(){
-		
-		//Definimos el layout:
-		ventana.setLayout(new GridLayout(1,2));
-		
-		bDialogo = new JButton("Selecciona Color");
-		ventana.add(bDialogo);
-	
-		panelColor = new JPanel();
-		ventana.add(panelColor);
-		
-	}
-	
-	/**
-	 * Método que inicializa todos los listeners del programa.
-	 */
-	public void inicializarListeners(){
-		bDialogo.addActionListener(e->{
-			DialogSelectorColor dialog = new DialogSelectorColor(bDialogo, panelColor.getBackground());
-			dialog.setVisible(true);
-			dialog.addWindowListener(new WindowAdapter() {
-				@Override
-				public void windowClosed(WindowEvent arg0) {
-					panelColor.setBackground(dialog.getColor());
-				}
-			});	
-		});
-		
-	}
-	
-
-	/**
-	 * Método que realiza todas las llamadas necesarias para inicializar la ventana correctamente.
-	 */
-	public void inicializar(){
-		ventana.setVisible(true);
-		inicializarComponentes();	
-		inicializarListeners();		
-	}
+    public VentanaPrincipal() {
+        ventana = new JFrame("High Score");
+        ventana.setBounds(400, 300, 900, 500);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setLayout(new GridBagLayout());
+    }
+    
+    public void inicializar() {
+        ventana.setVisible(true);
+        GridBagConstraints g1 = new GridBagConstraints();
+        g1.weightx = 1;
+        g1.weighty = 1;
+        g1.fill = GridBagConstraints.BOTH;
+        g1.gridx = 0;
+        g1.gridy = 0;
+        mitad1 = new JLabel("");
+        mitad1.setHorizontalAlignment(SwingConstants.CENTER);
+        mitad1.setVerticalAlignment(SwingConstants.CENTER);
+        mitad1.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        ventana.add(mitad1, g1);
+        GridBagConstraints g2 = new GridBagConstraints();
+        g2.weightx = 1;
+        g2.weighty = 10;
+        g2.fill = GridBagConstraints.BOTH;
+        g2.gridx = 0;
+        g2.gridy = 1;
+        mitad2 = new JLabel("");
+        mitad2.setHorizontalAlignment(SwingConstants.CENTER);
+        mitad2.setVerticalAlignment(SwingConstants.CENTER);
+        mitad2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        ventana.add(mitad2, g2);
+    }
 }
